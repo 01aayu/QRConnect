6005eb573d906ba2e340db7855c6233edd840b8a
01/aayu <shahaayush627@gmail.com>
2024-06-09 17:23:59 +0530

    comment out sqlalchemy

--- app.py
+++ app.py
@@ -165,10 +165,10 @@ else:
 
 
 # Configure session to use filesystem
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:@localhost/qrconnect"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:@localhost/qrconnect"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# db.init_app(app)
+db.init_app(app)
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
